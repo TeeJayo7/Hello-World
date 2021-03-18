68686500fca67334004c561d8fb8d975a82a8716
teejayo7 <joshuataiwo07@gmail.com>
2021-03-18 13:03:30 +0100

    add a background image style dashoard landing page

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./asset/img/w-profiler-logo.png">
+    <link rel="stylesheet" href="./asset/css/index.css">
+    <title>Profiler</title>
+</head>
+<body>
+
+    <h1 class="hello">Welcome to Profiler</h1>
+    <div>
+        
+        <strong>My Biography</strong> <br>
+    My name is Taiwo Joshua. I am a citizen of Nigeria. I come from Ogun State. I attended Potter's Place Nursery and Primary 
+       School for both my nursery and primary education. I then attended Easyway Internationl Model College for my Secondary
+       School education. I am yet to enter into a tertiary institution.
+   
+        As we all know, a biography is meant to contain life experiences and achievements, but presently, I have not gone 
+        through any life exoerience or achieved anything worth sharing. Check in later to this webpage for future updates. 
+        Thank you for your time.
+    </div>
+<center>
+    <a href="./content/registration.php">
+        <input type="button" value="Explore"/>
+    </a>
+</center>
+</body>
+</html>
